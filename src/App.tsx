d64fd6cfbb402c6ecbca32a88a6e42857aa8340c
ivan.andreski <ivan.andreski@gmail.com>
2023-12-09 18:35:28 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="text-7xl text-gray-400">
+      hello
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*'],
+      },
+      includeAssets: [
+        '**/*',
+      ],
+      registerType: 'autoUpdate',
+      manifest: {
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'food order',
+        description: 'app for planning food orders',
+        name: 'food order',
+        icons: [
+          {
+            'src': './src/assets/react.svg',
+            'type': 'image/svg'
+          }
+        ]
+      }
+    })
+  ],
+})
